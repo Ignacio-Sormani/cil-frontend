20b68d0c1692744572cb82b9db5042d5cb6b235a
Claudia V. Lima <limaclaudia@hotmail.com>
2020-03-07 18:58:04 -0300

    CIL-34: create footer

---
+++ src/components/layout/footer/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './footer.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className='footer'>
+        Trabajo Final MCGA - Ingeniería en Sistemas Informáticos - UAI Rosario
+      </div>
+    );
+  }
+}
+
+export default Header;
