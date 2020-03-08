98dbfd0b0fdf5767637c2e4afd580f8438f31c67
Ignacio Sormani <ignaciosormani@gmail.com>
2020-03-08 19:02:56 -0300

    CIL-39: configure redux store and persist

---
+++ src/redux/index.js
@@ -0,0 +1,24 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import auth from './auth/authReducer';
+
+const rootReducer = combineReducers({
+  auth
+});
+
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const store = createStore(
+  persistReducer(persistConfig, rootReducer),
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+const persistor = persistStore(store);
+
+export { store, persistor };
