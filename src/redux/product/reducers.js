8d00358fb71f950100e7f279d0bd124c95d0cbcb
Leonel Perez <leoperez8038@gmail.com>
2020-03-08 23:05:17 -0300

    CIL-47: create redux logic to get private products

--- src/redux/product/reducers.js
+++ src/redux/product/reducers.js
@@ -1,5 +1,8 @@
 import initialState from './initialState';
 import {
+  GET_PRODUCTS_FETCHING,
+  GET_PRODUCTS_FULFILLED,
+  GET_PRODUCTS_REJECTED,
   GET_ACTIVE_PRODUCTS_FETCHING,
   GET_ACTIVE_PRODUCTS_FULFILLED,
   GET_ACTIVE_PRODUCTS_REJECTED
@@ -7,25 +10,26 @@ import {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_PRODUCTS_FETCHING:
     case GET_ACTIVE_PRODUCTS_FETCHING:
       return {
         ...state,
         isFetching: true
       };
-    case GET_ACTIVE_PRODUCTS_FULFILLED: {
+    case GET_PRODUCTS_FULFILLED:
+    case GET_ACTIVE_PRODUCTS_FULFILLED:
       return {
         ...state,
         list: action.payload,
         isFetching: false
       };
-    }
-    case GET_ACTIVE_PRODUCTS_REJECTED: {
+    case GET_PRODUCTS_REJECTED:
+    case GET_ACTIVE_PRODUCTS_REJECTED:
       return {
         ...state,
         isFetching: false,
         error: action.error
       };
-    }
     default:
       return state;
   }
